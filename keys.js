2f8656094c754fe678988d0e43688718f300ed5d
Beverly Miramontes <beverly.miramontes@gmail.com>
2017-10-21 09:37:59 -0400

    initial site files

---
+++ keys.js
@@ -0,0 +1,19 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'mcRMkAfL1U5PONUE5gPemS0PX',
+  consumer_secret: 'ZzYuHlS4HvY2K77YBIqZ66LIMy2jKXiNUaPUuefNrRXNGfDyWO',
+  access_token_key: '2825466968-RGTjpaCqIk48MPLeeRegDBmiVUi1alAY4I5mmFn',
+  access_token_secret: 'Se0T6rbSNPe57zo7hrw5TXEmzDaaxeIyHCukJW3KfK9im',
+}
+
+
+var spotify = {
+  id: '71c6815c3d2949f081cf3520b55e8924',
+  secret: 'aad8be23881848ae9a23324528bdac33',
+}
+
+module.exports = {twitter: twitterKeys, 
+                  spotify: spotify};
+
+	
